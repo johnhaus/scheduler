7ffbb2b620682c74d0b489973c4f577d3797653c
johnhaus <john.haus@outlook.com>
2019-08-01 15:30:32 -0300

    entries are editable

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 
-import { without } from 'lodash';
+import { findIndex, without } from 'lodash';
 
 class App extends Component {
   constructor() {
@@ -24,6 +24,7 @@ class App extends Component {
     this.addAppointment = this.addAppointment.bind(this);
     this.changeOrder = this.changeOrder.bind(this);
     this.searchApts = this.searchApts.bind(this);
+    this.updateInfo = this.updateInfo.bind(this);
   }
 
   toggleForm() {
@@ -41,6 +42,17 @@ class App extends Component {
       orderBy: order,
       orderDir: dir
     });
+  }
+
+  updateInfo(name, value, id) {
+    let tempApts = this.state.myAppointments;
+    let aptIndex = findIndex(this.state.myAppointments, {
+      aptId: id
+    });
+    tempApts[aptIndex][name] = value;
+    this.setState({
+      myAppointments: tempApts
+    })
   }
 
   addAppointment(apt) {
@@ -131,7 +143,9 @@ class App extends Component {
                 />
                 <ListAppointments
                   appointments={filteredApts}
-                  deleteAppointment={this.deleteAppointment} />
+                  deleteAppointment={this.deleteAppointment}
+                  updateInfo={this.updateInfo}
+                />
               </div>
             </div>
           </div>
